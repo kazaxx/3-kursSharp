72498e94c2e1b90b7236df12f13855d70f5e9bc1
kazaxx <haligali025@gmail.com>
2023-10-04 22:23:19 +0300

    :100:

--- Радиус круга/Радиус круга/Program.cs
+++ Радиус круга/Радиус круга/Program.cs
@@ -1,7 +1,14 @@
-﻿public sealed class Circle
+﻿int radius;
+
+Console.WriteLine("Ведите радиус: ");
+radius = Convert.ToInt32(Console.ReadLine());
+
+double C = 2 * Math.PI * radius;
+Console.WriteLine("Длина окружности = " + C);
+
+public sealed class Circle
 {
     private double radius;
-
     public double Calculate(Func<double, double> op)
     {
         return op(radius);
