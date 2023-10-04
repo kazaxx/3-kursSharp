8ac8f83f81646aebbfbca62586ad6301a566707c
kazaxx <haligali025@gmail.com>
2023-10-04 21:50:25 +0300

    :100:

---
+++ Последовательность/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿int a;
+
+do
+{
+    Console.WriteLine("Введите целое число:");
+    if (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+    {
+        Console.WriteLine("Введите положительное целое число.");
+    }
+}
+
+while (a <= 0);
+
+for (int i = 1; i <= a; i++)
+{
+    Console.Write(i + " ");
+}
+
+for (int i = a - 1; i >= 1; i--)
+{
+    Console.Write(i + " ");
+}
+
+Console.WriteLine();
