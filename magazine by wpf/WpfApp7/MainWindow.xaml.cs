4fd9be40eb2f436f91f18e2e844b02786991b430
kazaxx <haligali025@gmail.com>
2024-02-12 22:41:07 +0300

    Начал делать front-end на wpf

---
+++ magazine by wpf/WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnAboutClicked(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
+
+        private void Button_OnDeliveryClicked(object sender, RoutedEventArgs e)
+        {
+            // Ваш код для действия "Доставка"...
+        }
+
+        private void Button_OnStoresClicked(object sender, RoutedEventArgs e)
+        {
+            // Ваш код для действия "Адреса магазинов"...
+        }
+
+        private void Button_OnAccountClicked(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+            this.Visibility = Visibility.Collapsed; // Скрыть текущую форму
+        }
+    }
+}
---
+++ magazine by wpf/WpfApp7/Window3.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Логика взаимодействия для Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void Window3_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Получаем элемент WebBrowser
+            WebBrowser webBrowser = (WebBrowser)sender;
+
+            // Отображаем веб-страницу Google Maps
+            webBrowser.Navigate("https://maps.app.goo.gl/kwRBeyjSLRA3gzg79");
+        }
+    }
+}
